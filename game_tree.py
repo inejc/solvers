fix a bug with terminal state eval and a bug with exploitablity calculation

game_tree.py
@@ -63,32 +63,30 @@ class Node:
             )
 
         opponent = 0 if player == 1 else 1
-
-        if self.state.history[-2:] == "bp":
-            # no showdown
-            if self.state.current_player == player:
-                # player won
-                payoff = self.state.bets[opponent]
-            else:
-                # opponent won
-                payoff = -self.state.bets[player]
-
-            return np.full(N_CARDS, fill_value=payoff)
-
-        # showdown
-        # possible hands are (J, Q, K)
         payoffs = np.zeros(N_CARDS)
 
+        # order of hands is (J, Q, K)
         for my_hand in range(N_CARDS):
             for opponent_hand in range(N_CARDS):
+
                 if my_hand == opponent_hand:
                     # blocker
                     continue
 
-                if my_hand > opponent_hand:
-                    payoff = self.state.bets[opponent]
+                if self.state.is_showdown():
+                    if my_hand > opponent_hand:
+                        # player won
+                        payoff = self.state.bets[opponent]
+                    else:
+                        # opponent won
+                        payoff = -self.state.bets[player]
                 else:
-                    payoff = -self.state.bets[player]
+                    if self.state.current_player == player:
+                        # player won
+                        payoff = self.state.bets[opponent]
+                    else:
+                        # opponent won
+                        payoff = -self.state.bets[player]
 
                 payoffs[my_hand] += \
                     (self.beliefs[opponent][opponent_hand] * payoff)

metrics.py
@@ -2,7 +2,7 @@ import copy
 
 import numpy as np
 
-from kuhn_poker import N_PLAYERS, N_CARDS
+from kuhn_poker import N_PLAYERS, N_ACTIONS, N_CARDS
 
 
 def calculate_exploitability(root_node):
@@ -72,9 +72,12 @@ def _set_best_response_strategy_as_current_strategy(node, player):
 
     if node.state.current_player == player:
         children_cfvs = np.array([child.cfvs for child in node.children])
-        best_action_i = np.argmax(children_cfvs, axis=0)
-        node.current_strategy[best_action_i, :] = 1.0
-        node.current_strategy[~best_action_i, :] = 0.0
+
+        row_i = np.argmax(children_cfvs, axis=0)
+        col_i = np.arange(row_i.shape[0])
+
+        node.current_strategy = np.zeros((N_ACTIONS, N_CARDS))
+        node.current_strategy[row_i, col_i] = 1.0
 
     for child in node.children:
         _set_best_response_strategy_as_current_strategy(child, player)